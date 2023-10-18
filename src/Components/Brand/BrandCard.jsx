fe1433624de514439d3707faca78f2f808947353
Bushra01-Dhaka <humayraanjum87@gmail.com>
2023-10-19 01:12:37 +0600

    Added dynamic route into each BrandCard

--- src/Components/Brand/BrandCard.jsx
+++ src/Components/Brand/BrandCard.jsx
@@ -1,35 +1,39 @@
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const BrandCard = ({ brand }) => {
-
-    useEffect(() => {
-        Aos.init();
-      }, []);
+  useEffect(() => {
+    Aos.init();
+  }, []);
 
   const { brand_name, brand_logo } = brand;
   return (
     <div>
-      <div data-aos="fade-up" data-aos-anchor-placement="top-center" className="card card-compact bg-base-100 shadow-xl rounded mb-6 hover:border-2 hover:border-black p-4">
-        <figure>
-          <img
-            className="h-[200px]"
-            src={brand_logo}
-            alt={brand_name}
-          />
-        </figure>
-        <div className="card-body">
-          <h1 className="text-center font-bold text-2xl text-black">{brand_name}</h1>
+      <Link to={`http://localhost:5000/brands/${brand_name}`}>
+        <div
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-center"
+          className="card card-compact bg-base-100 shadow-xl rounded mb-6 hover:border-2 hover:border-black p-4"
+        >
+          <figure>
+            <img className="h-[200px]" src={brand_logo} alt={brand_name} />
+          </figure>
+          <div className="card-body">
+            <h1 className="text-center font-bold text-2xl text-black">
+              {brand_name}
+            </h1>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
 
 BrandCard.propTypes = {
-    brand: PropTypes.object.isRequired
-}
+  brand: PropTypes.object.isRequired,
+};
 
 export default BrandCard;
